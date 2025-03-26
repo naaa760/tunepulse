59854731caf330bfebb0080414398a389f27a46e
nehhhhha6 <n0370525@gmail.com>
2025-03-26 17:31:32 +0530

    update

--- server/src/main.ts
+++ server/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 
   // Use the PORT environment variable provided by Render
   const port = process.env.PORT || 4000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on port: ${port}`);
 }
 bootstrap();
